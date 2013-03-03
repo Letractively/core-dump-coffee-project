52601d93988769ebb42f05d563f6d265d3bfd56a
lehanh.bk.mt1002@gmail.com <lehanh.bk.mt1002@gmail.com@e0cf70a0-78da-13be-ae49-ace207c431b2>
2013-03-03 03:21:20 +0000

    Khởi tạo csdl mysql

--- trunk/config/initializers/secret_token.rb
+++ trunk/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GoCoffee::Application.config.secret_token = '34c953bf2f20c7d91ed1b626e061e1fd976b799afe064d357d556d6991fe3da7599149989ca1f7c01d8a9a54b53f34654d3f5766f4f68626f26b4de820bf8814'
+CoffeShop::Application.config.secret_token = 'e2621213a427382261cf124bb28488828dff4764c434451aaf40000fc1be57800f65090875ea9b0ab0497aa5f76ab013a9314b4e6984e9bcb529386e848423b9'
--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -1,4 +1,4 @@
-GoCoffee::Application.routes.draw do
+CoffeShop::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
