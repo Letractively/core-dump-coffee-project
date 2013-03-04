e18fb4a5858ae529748d19f9467c5fcd8d0893f0
haonguyen1008 <haonguyen1008@gmail.com>
2013-03-04 05:22:59 +0000

    up lại từ đầu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoCoffee::Application.config.secret_token = '0ce938841806c654468a0156694347610a25f1bec67f9e955872f336399d6e469ca4433e09dbd51801915c746d58fe403cd57c6237744ed70ff0d4248ea45d1f'
