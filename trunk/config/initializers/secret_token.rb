276db25926a61c6ceb8cab88b13430e91c361ad0
haonguyen1008@gmail.com <haonguyen1008@gmail.com@e0cf70a0-78da-13be-ae49-ace207c431b2>
2013-03-04 05:08:33 +0000

    Removed trunk

--- trunk/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CoffeShop::Application.config.secret_token = 'e2621213a427382261cf124bb28488828dff4764c434451aaf40000fc1be57800f65090875ea9b0ab0497aa5f76ab013a9314b4e6984e9bcb529386e848423b9'
